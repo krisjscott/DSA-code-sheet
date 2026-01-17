13d47c434a36b26d8a2dff0ba714afa0ab112d16
Krish Kumar <krishkumar6566@gmail.com>
2026-01-17 10:40:05 +0530

    More functions

--- Recusrion/recursion.py
+++ Recusrion/recursion.py
@@ -12,5 +12,12 @@ def fibbonaci(n):
         return 1
     fib = fibbonaci(n-1) + fibbonaci(n-2)
     return fib
+def sum_of_first(value):
+    sum = value
+    if value == 1:
+        return 1
+    sum = sum + sum_of_first(value-1)
+    return sum
 print(factorial(5))
 print(fibbonaci(7))
+print(sum_of_first(10))

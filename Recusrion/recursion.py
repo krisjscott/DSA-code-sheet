b5abed243a8a195b8bddd35d3f7e807ba9606218
Krish Kumar <krishkumar6566@gmail.com>
2026-01-18 18:58:59 +0530

    Added more functions for practice

--- Recusrion/recursion.py
+++ Recusrion/recursion.py
@@ -7,17 +7,37 @@ def factorial(n):
         return 1 
     fact = n * factorial(n-1)
     return fact
+
 def fibbonaci(n):
     if n == 1 or n == 2:
         return 1
     fib = fibbonaci(n-1) + fibbonaci(n-2)
     return fib
+
 def sum_of_first(value):
     sum = value
     if value == 1:
         return 1
     sum = sum + sum_of_first(value-1)
     return sum
+
+def power(value, x):
+    if value == 1 or x == 1:
+        return value
+    elif x == 0:
+        return 1
+    total = power(value,x-1)*value
+    return total
+
+def count_down(value):
+    if value < 0:
+        return 
+    print(value)
+    count_down(value-1)
+
+    
 print(factorial(5))
 print(fibbonaci(7))
 print(sum_of_first(10))
+print(power(-2,4))
+print(count_down(4))

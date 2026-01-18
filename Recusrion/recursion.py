b25723d2e78e3ddbcd154889b830340da8824e20
Krish Kumar <krishkumar6566@gmail.com>
2026-01-18 23:11:16 +0530

    Fix recursion base calls and functions

--- Recusrion/recursion.py
+++ Recusrion/recursion.py
@@ -35,9 +35,26 @@ def count_down(value):
     print(value)
     count_down(value-1)
 
+def triangular_number(n):
+    if n == 1:
+        return 1
+    total = n + triangular_number(n-1) 
+    return total
+
+def fibbonaci_vari(n):
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 3
+    new=fibbonaci_vari(n-1)+fibbonaci_vari(n-2)
+    return new
+     
+    
     
 print(factorial(5))
 print(fibbonaci(7))
 print(sum_of_first(10))
 print(power(-2,4))
 print(count_down(4))
+print(triangular_number(4))
+print(fibbonaci_vari(4))
